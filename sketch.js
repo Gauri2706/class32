d7040dfde2509b9d5a1e5c6a6c4c8f0eb8ce772d
Gauri2706 <visputegauri2@gmail.com>
2021-05-18 14:27:14 +0530

    debugging

--- sketch.js
+++ sketch.js
@@ -7,11 +7,14 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
+var score = 0;
 
 var gameState = "onSling";
+var bg = "sprites/bg.png";
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+    getBackgroundImage();
+
 }
 
 function setup(){
@@ -45,18 +48,30 @@ function setup(){
 }
 
 function draw(){
+
+    if(backgroundImg)
+
     background(backgroundImg);
     Engine.update(engine);
+
+    noStroke();
+    textSize(35);
+    fill(255);
+    text("score:"+score,1000, 50);
+
+
     //strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
     pig1.display();
+    pig1.score();
     log1.display();
 
     box3.display();
     box4.display();
     pig3.display();
+    pig3.score();  
     log3.display();
 
     box5.display();
@@ -70,9 +85,9 @@ function draw(){
 }
 
 function mouseDragged(){
-    if (gameState!=="launched"){
+   // if (gameState!=="launched"){
         Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
-    }
+    //}
 }
 
 
@@ -83,6 +98,32 @@ function mouseReleased(){
 
 function keyPressed(){
     if(keyCode === 32){
-       // slingshot.attach(bird.body);
+      slingshot.attach(bird.body);
+      bird.trajectory = [];
+      Matter.Body.setPosition(bird.body, {x: 200 , y: 50});
     }
+}
+
+async function getBackgroundImage(){
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+
+    var responseJSON = await response.json();
+    var datetime = responseJSON.datetime;
+
+    console.log(responseJSON);
+    console.log(datetime);
+
+    var hour = datetime.slice(11,13);
+    console.log(hour);
+
+    if(hour >= 06 && hour <= 13){
+        bg = "sprites/bg.png";
+    }
+      else{
+         bg = "sprites/bg2.jpg"; 
+      }
+
+    backgroundImg = loadImage(bg);
+    console.log(backgroundImg);
+
 }
